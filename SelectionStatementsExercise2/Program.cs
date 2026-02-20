bd5112d23d6a085c756a73f5ff98d7b1fdc8495d
Ruben Gonzalez <rubengz23@yahoo.com>
2026-02-19 22:37:01 -0800

    Assignment part 2 save

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            string favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject.ToLower())
+            {
+                case "math":
+                    case "mathematics":
+                        Console.WriteLine("Math huh, you must be smart.");
+                        break;
+                case "science":
+                    Console.WriteLine("You a science nerd?");
+                    break;
+                case "english":
+                    Console.WriteLine("Me LikE to ReaD 2.");
+                    break;
+                case "history":
+                    Console.WriteLine("History? Like Dinosaurs?");
+                    break;
+                case "pe":
+                    case "physical eduction":
+                        Console.WriteLine("No Way, me too!");
+                        break;
+                default:
+                    Console.WriteLine($"{favoriteSubject}? Never heard of it.");
+                    break;
+            }
+
         }
     }
 }
